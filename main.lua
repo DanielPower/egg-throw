6612bf46389585380726d32348f08fced45ff4d7
Daniel Power <me@danielpower.ca>
2024-07-09 23:34:17 -0230

    Add camera zooming

--- main.lua
+++ main.lua
@@ -7,30 +7,36 @@ local camera
 local floor
 
 function love.load()
-  camera = Camera()
-  floor = Floor()
-  camera.scale = 5
+	camera = Camera()
+	floor = Floor()
+	camera.scale = 5
 end
 
 function love.update(dt)
-  if love.keyboard.isDown("up") then
-    camera:move(0, -100 * dt)
-  end
-  if love.keyboard.isDown("down") then
-    camera:move(0, 100 * dt)
-  end
-  if love.keyboard.isDown("left") then
-    camera:move(-100 * dt, 0)
-  end
-  if love.keyboard.isDown("right") then
-    camera:move(100 * dt, 0)
-  end
+	if love.keyboard.isDown("up") then
+		camera:move(0, -100 * dt)
+	end
+	if love.keyboard.isDown("down") then
+		camera:move(0, 100 * dt)
+	end
+	if love.keyboard.isDown("left") then
+		camera:move(-100 * dt, 0)
+	end
+	if love.keyboard.isDown("right") then
+		camera:move(100 * dt, 0)
+	end
+	if love.keyboard.isDown("-") then
+		camera.scale = camera.scale * 0.5 ^ dt
+	end
+	if love.keyboard.isDown("=") then
+		camera.scale = camera.scale * 2 ^ dt
+	end
 end
 
 function love.draw()
-  love.graphics.setColor(1, 1, 1)
-  love.graphics.clear(0.53, 0.81, 0.92)
-  camera:draw(function()
-    floor:draw(camera)
-  end)
+	love.graphics.setColor(1, 1, 1)
+	love.graphics.clear(0.53, 0.81, 0.92)
+	camera:draw(function()
+		floor:draw(camera)
+	end)
 end
